ff2c260ee7fd2c8f084745e9b35ddac5ab64f543
crescobar95 <cristian.escobar95@outlook.es>
2025-03-01 11:09:31 -0500

    se modifico .env

--- config/config.js
+++ config/config.js
@@ -1,31 +1,30 @@
-import Dotenv from "dotenv";
-Dotenv.config();
+import "dotenv/config";
 
-export default{
+export default {
   development: {
-    username: "postgres",
-    password: "cristian",
-    database: "guayas",
-    host: "db",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "cristian",
+    database: process.env.DB_NAME || "guayas",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres"
   },
 
   test: {
-   username: "postgres",
-    password: "cristian",
-    database: "database_test",
-    host: "db",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "cristian",
+    database: process.env.DB_NAME || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres"
   },
-  productio:{
-  use_env_variable: "database_url",
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true, // Necesario en Render
-      rejectUnauthorized: false
+
+  production: {
+    use_env_variable: "postgresql://postgresdbguayas_user:tA3SbsmZrW6yyVV1a4am1DS5OyLkUaiU@dpg-cuvvj22j1k6c738ak3qg-a.oregon-postgres.render.com/postgresdbguayas",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
-  }
-}
+};
